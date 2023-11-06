33ee55667a4a7333a7beabfac16246f418a4276c
dhavalpatel2575 <71452353+dhavalpatel2575@users.noreply.github.com>
2023-11-05 19:06:21 -0500

    error solved

--- Main.java
+++ Main.java
@@ -1,9 +1,9 @@
-public static Main()
+public class Main()
 {
         public static void main(String[] args)
         {
             System.out.println("this is java program");
-            
+               
         }
 
 
